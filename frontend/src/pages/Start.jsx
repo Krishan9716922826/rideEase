118d26769b7768c670b3e977ed464afc273e44f2
Krishan Kumar 20 <krishan9716922826@gmail.com>
2026-05-31 09:42:41 +0530

    fix

--- frontend/src/pages/Start.jsx
+++ frontend/src/pages/Start.jsx
@@ -4,8 +4,10 @@ import { Link } from 'react-router-dom'
 const Start = () => {
   return (
     <div>
-      <div className='bg-cover bg-center bg-[url(https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_768,w_1152/v1731611146/assets/2f/4d92c3-9852-42a4-9c50-96e969434dbe/original/Uber-404-desktop-sq.png)] h-screen pt-8 flex justify-between flex-col w-full'>
-        <img className='w-16 ml-8' src="https://cdn-assets-eu.frontify.com/s3/frontify-enterprise-files-eu/eyJwYXRoIjoid2VhcmVcL2ZpbGVcLzhGbTh4cU5SZGZUVjUxYVh3bnEyLnN2ZyJ9:weare:F1cOF9Bps96cMy7r9Y2d7affBYsDeiDoIHfqZrbcxAw?width=1200&height=417" alt="" />
+      <div className='h-screen flex justify-between flex-col w-full'>
+        <div className='relative flex-1 bg-cover bg-center md:bg-top bg-[url(https://cn-geo1.uber.com/image-proc/crop/resizecrop/udam/format=auto/width=1152/height=768/srcb64=aHR0cHM6Ly90Yi1zdGF0aWMudWJlci5jb20vcHJvZC91ZGFtLWFzc2V0cy8yZjRkOTJjMy05ODUyLTQyYTQtOWM1MC05NmU5Njk0MzRkYmUucG5n)]'>
+          <img className='w-16 ml-8 pt-8' src="https://cdn-assets-eu.frontify.com/s3/frontify-enterprise-files-eu/eyJwYXRoIjoid2VhcmVcL2ZpbGVcLzhGbTh4cU5SZGZUVjUxYVh3bnEyLnN2ZyJ9:weare:F1cOF9Bps96cMy7r9Y2d7affBYsDeiDoIHfqZrbcxAw?width=1200&height=417" alt="Uber" />
+        </div>
         <div className='bg-white pb-8 py-4 px-4'>
           <h2 className='text-[30px] font-semibold'>Get Started with Uber</h2>
           <Link to='/login' className='flex items-center justify-center w-full bg-black text-white py-3 rounded-lg mt-5'>Continue</Link>
